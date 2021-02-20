b12b56a8f9b0b41191989c5d41d643e9a077d084
sebastian <sebastian bowen>
2021-02-20 15:17:36 -0700

    done

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -17,6 +17,7 @@ var tasks = {};
     $(taskEl).addClass("list-group-item-danger")
   }else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
+    console.log(taskEl);
   }
   
 };
@@ -80,7 +81,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -216,15 +217,26 @@ loadTasks();
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+     $(this).addClass("dropover");
+     $(".bottom-trash").addClass("bottom-trash-drag");
+
     
   },
   deactivate: function(event) {
+$(this).removeClass("dropover");  
+$(".bottom-trash").removeClass("bottom-trash-drag");  
+
+  
+    
    
   },
   over: function(event) {
+   $(event.target).addClass("dropover-active");
+
    
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active");
    
   },
   update: function(event) {
@@ -268,12 +280,14 @@ loadTasks();
     drop: function(event, ui) {
       console.log("drop");
       ui.draggable.remove();
+      $(".bottom-trash").removeClass("bottom-trash-active");
     },
     over: function(event, ui) {
-      console.log("over");
+      $(".bottom-trash").addClass("bottom-trash-active");
+
     },
     out: function(event, ui) {
-      console.log("out");
+     $(".bottom-trash").removeClass("bottom-trash-activate");
     }
   });
 
@@ -282,10 +296,8 @@ loadTasks();
     minDate: 1
   });
 
- 
-
-
-
-
-
-
+setInterval(function() {
+  $(".card .list-group-item").each(function(index,el){
+    auditTask(el);
+  });
+}, (1000 * 60)* 30);
